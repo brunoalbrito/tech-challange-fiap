f9fc41554c7418f086e18b5ac396610421d90aa6
Bruno Al. Brito <brunoalbuquerquebrito@outlook.com>
2023-10-20 12:13:58 -0300

    Passing id as param (#20)

--- src/main/java/br/com/fiap/techchallenge/infrastructure/postgress/UserRepositoryPostgress.java
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.techchallenge.infrastructure.postgress;
-
-import br.com.fiap.techchallenge.infrastructure.entity.User;
-import br.com.fiap.techchallenge.infrastructure.repository.UserRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepositoryPostgress extends UserRepository, JpaRepository<User, Long> {
-}
