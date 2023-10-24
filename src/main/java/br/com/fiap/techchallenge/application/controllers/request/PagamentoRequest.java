c2a809af14871168ba21a1018173115dc0ba0149
abraz <alcidesbraz@hotmail.com>
2023-10-23 22:42:17 -0300

    removendo anotacoes do lombok

--- src/main/java/br/com/fiap/techchallenge/application/controllers/request/PagamentoRequest.java
+++ src/main/java/br/com/fiap/techchallenge/application/controllers/request/PagamentoRequest.java
@@ -1,15 +1,5 @@
 package br.com.fiap.techchallenge.application.controllers.request;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@Builder
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PagamentoRequest {
-    //TODO: implementar
 }
