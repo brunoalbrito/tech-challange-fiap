a00de84db7302d5757cd4a81d3ec7a0035bbff82
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2023-10-30 17:47:11 -0700

    Fixing insert of data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,32 +1,32 @@
--- Enable the uuid-ossp extension
-CREATE
-EXTENSION IF NOT EXISTS "uuid-ossp";
+INSERT INTO public.clientes (cpf) VALUES
+	 ('70262120011');
 
--- Insert data into 'ingredientes' table
-INSERT INTO public.ingredientes (id, descricao)
-VALUES (uuid_generate_v4(), 'Ingredient 1'),
-       (uuid_generate_v4(), 'Ingredient 2');
--- Add more rows as needed;
+INSERT INTO public.ingredientes (id,descricao) VALUES
+	 ('d4880892-6b5d-490a-be48-eac8bc3370ba','hamburguer'),
+	 ('80b3a73c-eb86-45ad-9f8b-599bf3f09a0e','morango');
 
--- Insert data into 'produtos' table
-INSERT INTO public.produtos (id, descricao, nome, preco, tipo)
-VALUES (uuid_generate_v4(), 'Product 1', 'Product 1 Name', 10.99, 'LANCHE'),
-       (uuid_generate_v4(), 'Product 2', 'Product 2 Name', 15.99, 'BEBIDA');
--- Add more rows as needed;
+INSERT INTO public.produtos (id,descricao,nome,preco,tipo) VALUES
+	 ('6444030b-67ab-42d5-83a0-3f70ed358cdd','Lanche com Bacon','XBacon',8.00,'LANCHE'),
+	 ('59b30987-dec4-4eee-a891-403366d472de','Vitamina de Morango','Suco de Morango',15.00,'BEBIDA'),
+	 ('67d9da33-509f-4e13-b8d1-49d535c32cc3','Bolo de Morango','Bolo',15.00,'ACOMPANHAMENTO');
 
--- Insert data into 'produtos_ingredientes' table
-INSERT INTO public.produtos_ingredientes (produto_entity_id, ingredientes_id)
-VALUES ((SELECT id FROM public.produtos ORDER BY random() LIMIT 1),
-       (SELECT id FROM public.ingredientes ORDER BY random() LIMIT 1) );
--- Add more rows as needed;
+INSERT INTO public.produtos_ingredientes (produto_entity_id,ingredientes_id) VALUES
+	 ('67d9da33-509f-4e13-b8d1-49d535c32cc3','80b3a73c-eb86-45ad-9f8b-599bf3f09a0e'),
+	 ('6444030b-67ab-42d5-83a0-3f70ed358cdd','d4880892-6b5d-490a-be48-eac8bc3370ba'),
+	 ('59b30987-dec4-4eee-a891-403366d472de','80b3a73c-eb86-45ad-9f8b-599bf3f09a0e');
 
--- Insert data into 'combos' table
-INSERT INTO public.combos (id)
-VALUES (uuid_generate_v4());
--- Add more rows as needed;
+INSERT INTO public.combos (id) VALUES
+	 ('73f25cde-6e23-4c5c-a18f-e148bf2c9307');
 
--- Insert data into 'combos_produtos' table
-INSERT INTO public.combos_produtos (combo_entity_id, produtos_id)
-VALUES ((SELECT id FROM public.combos ORDER BY random() LIMIT 1),
-       (SELECT id FROM public.produtos ORDER BY random() LIMIT 1) )
--- Add more rows as needed;
+INSERT INTO public.combos_produtos (combo_entity_id,produtos_id) VALUES
+	 ('73f25cde-6e23-4c5c-a18f-e148bf2c9307','6444030b-67ab-42d5-83a0-3f70ed358cdd'),
+	 ('73f25cde-6e23-4c5c-a18f-e148bf2c9307','59b30987-dec4-4eee-a891-403366d472de'),
+	 ('73f25cde-6e23-4c5c-a18f-e148bf2c9307','67d9da33-509f-4e13-b8d1-49d535c32cc3');
+
+INSERT INTO public.pedidos (id,payment_id,payment_qr_code,status,client_id) VALUES
+	 ('60e54eb2-9a8e-49f7-8f69-a177abc4feb1','be9dba14-cd1c-4bf0-8bcb-fa734d70feb5','d2962ef7-0caa-44b2-acfe-ebd2354cac3e','ENTREGUE','70262120011');
+
+INSERT INTO public.pedidos_produtos (pedido_entity_id,produtos_id) VALUES
+	 ('60e54eb2-9a8e-49f7-8f69-a177abc4feb1','6444030b-67ab-42d5-83a0-3f70ed358cdd'),
+	 ('60e54eb2-9a8e-49f7-8f69-a177abc4feb1','59b30987-dec4-4eee-a891-403366d472de');
+
