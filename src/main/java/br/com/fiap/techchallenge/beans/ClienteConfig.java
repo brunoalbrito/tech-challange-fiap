d700b1a0d46cc3292d60c0c42f9f3901e9675264
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2024-01-26 23:02:53 -0300

    Removing EntityMapper to make implementation easier

--- src/main/java/br/com/fiap/techchallenge/beans/ClienteConfig.java
+++ src/main/java/br/com/fiap/techchallenge/beans/ClienteConfig.java
@@ -4,7 +4,6 @@ import br.com.fiap.techchallenge.application.gateways.ClienteGateway;
 import br.com.fiap.techchallenge.application.usecases.cliente.CreateClienteInteractor;
 import br.com.fiap.techchallenge.infrastructure.controllers.ClienteDTOMapper;
 import br.com.fiap.techchallenge.infrastructure.persistence.gateways.ClienteRepositoryGateway;
-import br.com.fiap.techchallenge.infrastructure.persistence.mappers.ClienteEntityMapper;
 import br.com.fiap.techchallenge.infrastructure.persistence.repository.ClienteRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,18 +11,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ClienteConfig {
     @Bean
-    CreateClienteInteractor createClienteCase(ClienteGateway clienteGateway) {
+    CreateClienteInteractor createClienteInteractor(ClienteGateway clienteGateway) {
         return new CreateClienteInteractor(clienteGateway);
     }
 
     @Bean
-    ClienteGateway clienteGateway(ClienteRepository clienteRepository, ClienteEntityMapper clienteEntityMapper) {
-        return new ClienteRepositoryGateway(clienteRepository, clienteEntityMapper);
-    }
-
-    @Bean
-    ClienteEntityMapper clienteEntityMapper() {
-        return new ClienteEntityMapper();
+    ClienteGateway clienteGateway(ClienteRepository clienteRepository) {
+        return new ClienteRepositoryGateway(clienteRepository);
     }
 
     @Bean
--- src/main/java/br/com/fiap/techchallenge/infrastructure/persistence/gateways/ClienteRepositoryGateway.java
+++ src/main/java/br/com/fiap/techchallenge/infrastructure/persistence/gateways/ClienteRepositoryGateway.java
@@ -2,24 +2,20 @@ package br.com.fiap.techchallenge.infrastructure.persistence.gateways;
 
 import br.com.fiap.techchallenge.application.gateways.ClienteGateway;
 import br.com.fiap.techchallenge.domain.Cliente;
-import br.com.fiap.techchallenge.infrastructure.persistence.mappers.ClienteEntityMapper;
 import br.com.fiap.techchallenge.infrastructure.persistence.entity.ClienteEntity;
 import br.com.fiap.techchallenge.infrastructure.persistence.repository.ClienteRepository;
 
 public class ClienteRepositoryGateway implements ClienteGateway {
     private final ClienteRepository clienteRepository;
-    private final ClienteEntityMapper clienteEntityMapper;
 
-    public ClienteRepositoryGateway(ClienteRepository clienteRepository, ClienteEntityMapper clienteEntityMapper) {
+    public ClienteRepositoryGateway(ClienteRepository clienteRepository) {
         this.clienteRepository = clienteRepository;
-        this.clienteEntityMapper = clienteEntityMapper;
     }
 
     @Override
-    public Cliente criaCliente(Cliente clienteDomainObj) {
-        ClienteEntity clienteEntity = clienteEntityMapper.toEntity(clienteDomainObj);
-        ClienteEntity savedObj = clienteRepository.save(clienteEntity);
-        return clienteEntityMapper.toDomainObj(savedObj);
+    public Cliente criaCliente(Cliente cliente) {
+        ClienteEntity clienteEntity = ClienteEntity.criaEntity(cliente);
+        return clienteRepository.save(clienteEntity).toDomain();
 
     }
 }
