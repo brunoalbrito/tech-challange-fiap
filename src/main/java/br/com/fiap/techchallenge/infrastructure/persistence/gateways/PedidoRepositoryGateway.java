eed2d035bc1c1750b14f64a4f6ae9d619031136a
brunoalbrito <brunoalbuquerquebrito@outlook.com>
2024-01-29 20:41:11 -0300

    Lista pedidos

--- src/main/java/br/com/fiap/techchallenge/infrastructure/persistence/gateways/PedidoRepositoryGateway.java
+++ src/main/java/br/com/fiap/techchallenge/infrastructure/persistence/gateways/PedidoRepositoryGateway.java
@@ -2,9 +2,11 @@ package br.com.fiap.techchallenge.infrastructure.persistence.gateways;
 
 import br.com.fiap.techchallenge.application.gateways.PedidoGateway;
 import br.com.fiap.techchallenge.domain.Pedido;
+import br.com.fiap.techchallenge.domain.enums.StatusPedido;
 import br.com.fiap.techchallenge.infrastructure.persistence.entity.PedidoEntity;
 import br.com.fiap.techchallenge.infrastructure.persistence.repository.PedidoRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public class PedidoRepositoryGateway implements PedidoGateway {
@@ -23,5 +25,13 @@ public class PedidoRepositoryGateway implements PedidoGateway {
     public Pedido salva(Pedido pedido) {
         PedidoEntity pedidoEntity = PedidoEntity.toEntity(pedido);
         return pedidoRepository.save(pedidoEntity).toDomain();
+    }
+
+    @Override
+    public List<Pedido> listaPedido() {
+        return pedidoRepository.findAll().stream()
+                .filter(pedido -> !pedido.getStatus().equals(StatusPedido.ENTREGUE))
+                .map(PedidoEntity::toDomain)
+                .toList();
     }
 }
